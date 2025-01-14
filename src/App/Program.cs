6da66233d60fa49d58c6c71317e97cc31254e253
Tyler Kendrick <145080887+Tyler-R-Kendrick@users.noreply.github.com>
2025-01-14 03:20:05 +0000

    initial commit of agent code

--- src/App/Program.cs
+++ src/App/Program.cs
@@ -1,6 +1,9 @@
+using System.ComponentModel;
+using Microsoft.Extensions.AI;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Data;
+using OpenAI.Chat;
 
 HostApplicationBuilder builder = new();
 
@@ -24,24 +27,79 @@ Console.ReadLine(); // Wait for user input before closing
 
 await host.RunAsync();
 
-class FodmapResearchPlugin(ITextSearch textSearch)
+class FodmapResearchPlugin(IChatClient chatClient)
 {
-    [KernelFunction("Research FODMAPs")]
-    public async Task<TextSearchResult[]> ResearchFodmapsAsync(
-        string foodName)
+    [Description("The level at which a food sensitivity/intolerance is experienced.")]
+    public enum IntoleranceLevel
     {
-        var prompt = $"Is {foodName} a FODMAP? If so, provide a brief summary of the food related to its FODMAP status.";
-        var results = await textSearch.GetTextSearchResultsAsync(prompt, new()
-        {
-            Top = 4
-        });
-        List<TextSearchResult> snippets = [];
-#pragma warning restore SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+        [Description("High sensitivity")]
+        High,
+        [Description("Moderate sensitivity")]
+        Moderate,
+        [Description("Low sensitivity")]
+        Low,
+        [Description("No detectable sensitivity")]
+        None
+    }
 
-        await foreach (var result in results.Results)
+    [Flags, Description("The categories of food sensitivities for classification.")]
+    public enum SensitivityCategory : long
+    {
+        [Description("No sensitivity")]
+        None = 0,
+        [Description("Fructans")]
+        Fructans,
+        [Description("Ogliosaccharides")]
+        Ogliosaccharides,
+        [Description("Disaccharides")]
+        Disaccharides,
+        [Description("Monosaccharides")]
+        Monosaccharides,
+        [Description("Polyols")]
+        Polyols,
+        [Description("Dairy")]
+        Dairy,
+        [Description("Gluten")]
+        Gluten,
+    }
+
+    [Description("The level of sensitivity to a particular category of food.")]
+    public record SensitivityLevel(
+        [Description("The category of food sensitivity.")]
+        SensitivityCategory Sensitivity,
+        [Description("The level of intolerance to the food category.")]
+        IntoleranceLevel IntoleranceLevel);
+    
+    [Description("The food name and its associated sensitivity levels.")]
+    public record FoodSensitivity(
+        [Description("The name of the food being researched.")]
+        string FoodName,
+        [Description("The sensitivity levels for the food.")]
+        SensitivityLevel[] SensitivityLevels);
+
+    [Description("Research Food sensitivities for a given food name.")]
+    public async Task<FoodSensitivity> ResearchFoodSensitivityAsync(
+        [Description("The name of the food being researched.")] string foodName)
+    {
+        var sensitivityCategoryNames = Enum.GetNames<SensitivityCategory>();
+        var intoleranceLevelNames = Enum.GetNames<IntoleranceLevel>();
+        var intoleranceLevelPrompt = $"Choose from {string.Join(", ", intoleranceLevelNames)}";
+        Task<ChatCompletion<T>> Search<T>(string prompt) => chatClient.CompleteAsync<T>(prompt);
+        async Task<SensitivityLevel> GetSensitivityLevel(string categoryName)
         {
-            snippets.Add(result);
+            var prompt = $"What is the intolerance level for {categoryName}? {intoleranceLevelPrompt}";
+            var levelCompletion = await Search<IntoleranceLevel>(prompt);
+            var level = levelCompletion.Result;
+            var category = Enum.Parse<SensitivityCategory>(categoryName);
+            return new(category, level);
         }
-        return [.. snippets];
+        var sensitivities = await sensitivityCategoryNames
+            .ToAsyncEnumerable()
+            .SelectAwait(async x => await GetSensitivityLevel(x))
+            .Where(x => ((int)x.Sensitivity & (~0)) != 0)
+            .Where(x => x.IntoleranceLevel != IntoleranceLevel.None)
+            .ToArrayAsync();
+
+        return new(foodName, sensitivities);
     }
 }
